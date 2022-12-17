da9592689969bff86d3bc0f17535f94ab275831d
Yann-Brault <brlt.yann@gmail.com>
2022-12-17 11:17:33 +0100

    cleaning

--- library/src/model/index.ts
+++ library/src/model/index.ts
@@ -1,6 +1,5 @@
 export * from "./application.class";
 export * from "./utils/index";
 export * from "./states/index";
-export * from "./commands/index";
 export * from "./bricks/index";
 export * from "./block/index";
